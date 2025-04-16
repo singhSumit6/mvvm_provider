2e8815ff14a2e0804bb1322e639fdf7592e344a6
singSumit6 <rajputhrithik036@gmail.com>
2025-04-16 22:33:33 +0530

    Route Added

---
+++ lib/utils/routes/routes_name.dart
@@ -0,0 +1,5 @@
+class RoutesName{
+
+   static const login = 'login';
+   static const home = 'home';
+}
---
+++ lib/view/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_provider/utils/routes/routes_name.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Center(
+          child: TextButton(
+              onPressed: () => Navigator.pushNamed(context, RoutesName.home),
+              child: const Text("Click Me "))),
+    );
+  }
+}
